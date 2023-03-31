65678caf1dea60dd82018a921c86ced5677d9f7b
MElkmeshi <mohamed2108926@miuegypt.edu.eg>
2023-03-31 04:37:21 +0200

    test

--- app.py
+++ app.py
@@ -17,10 +17,10 @@ class FileModified():
                 if self.callback():
                     break
 
-dir_path = "/path/to/directory"
-
+dir_path = "C:\\Users\\melkmeshi\\Documents\\Projects\\Python\\git"
 for file_name in os.listdir(dir_path):
     if os.path.isfile(os.path.join(dir_path, file_name)):
         file_path = os.path.join(dir_path, file_name)
+        print(file_path)
         file = FileModified(file_path, lambda: os.system('cmd /c "git add . && git commit -m "test" && git push orgin master"'))
         file.start()
