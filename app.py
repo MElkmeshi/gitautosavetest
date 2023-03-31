8b7d4cfef6c35f8cf7ecda7fd13d1b9a4564fd3f
MElkmeshi <mohamed2108926@miuegypt.edu.eg>
2023-03-31 04:39:49 +0200

    test

--- app.py
+++ app.py
@@ -1,26 +1,32 @@
 import os
 import time
+import traceback
 import threading
 
-class FileModified():
+class FileModified(threading.Thread):
     def __init__(self, file_path, callback):
+        threading.Thread.__init__(self)
         self.file_path = file_path
         self.callback = callback
         self.modifiedOn = os.path.getmtime(file_path)
-    def start(self):
-        while (True):
+        self.stop_event = threading.Event()
+
+    def run(self):
+        while not self.stop_event.is_set():
             time.sleep(0.5)
             modified = os.path.getmtime(self.file_path)
             if modified != self.modifiedOn:
                 self.modifiedOn = modified
                 if self.callback():
-                    break
+                    self.stop_event.set()
 
-dir_path = "C:\\Users\\melkmeshi\\Documents\\Projects\\Python\\git"
+    def stop(self):
+        self.stop_event.set()
+
+dir_path = "/path/to/directory"
 
 for file_name in os.listdir(dir_path):
     if os.path.isfile(os.path.join(dir_path, file_name)):
         file_path = os.path.join(dir_path, file_name)
-        print(file_path)
         file = FileModified(file_path, lambda: os.system('cmd /c "git add . && git commit -m "test" && git push orgin master"'))
         file.start()
